d4821016ed2185149e2cd67b425d79342277c57e
To'Iki Rouillard <trouilla@fu-r8-p3.s19.be>
2024-12-17 16:01:38 +0100

    all

---
+++ pipex/pipex.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trouilla <trouilla@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/17 10:04:02 by trouilla          #+#    #+#             */
+/*   Updated: 2024/12/17 14:30:51 by trouilla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <sys/wait.h>
+# include <errno.h>
+
+
+
+#endif 
