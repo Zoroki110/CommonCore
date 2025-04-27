d5e53f6ab32f052eb2fa4c516f5bd6f02749552f
To'Iki Rouillard <trouilla@shi-r6-p6.s19.be>
2025-04-27 08:26:48 +0200

    ex01 valid

--- CPP01/ex01/main.cpp
+++ CPP01/ex01/main.cpp
@@ -6,15 +6,15 @@ int main() {
     std::string* stringPTR = &brain;
     std::string& stringREF = brain;
 
-    std::cout << "Address of brain:    " << &brain << std::endl;
-    std::cout << "Address held by PTR: " << stringPTR << std::endl;
-    std::cout << "Address held by REF: " << &stringREF << std::endl;
+    std::cout << "Address de brain:       " << &brain << std::endl;
+    std::cout << "Address pointe par PTR: " << stringPTR << std::endl;
+    std::cout << "Address pointe par REF: " << &stringREF << std::endl;
 
     std::cout << "-------------------------------------" << std::endl;
 
-    std::cout << "Value of brain:       " << brain << std::endl;
-    std::cout << "Value pointed by PTR: " << *stringPTR << std::endl;
-    std::cout << "Value referenced by REF: " << stringREF << std::endl;
+    std::cout << "Valeur de brain:         " << brain << std::endl;
+    std::cout << "Valeur pointe de PTR:    " << *stringPTR << std::endl;
+    std::cout << "Valeur reference de REF: " << stringREF << std::endl;
 
     return 0;
 }
